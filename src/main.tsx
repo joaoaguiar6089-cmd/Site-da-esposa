109b3f198817e24782e249c0434d2c65340869a5
joaoaguiar6089-cmd <joaoaguiar6089@gmail.com>
2025-10-08 03:53:22 -0400

    fix0353

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from './components/auth/AuthContext.tsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -12,6 +13,8 @@ import AreaClientePage from "./pages/AreaCliente.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import CategoryProcedures from "./pages/CategoryProcedures.tsx";
 import SubcategoryProcedures from "./pages/SubcategoryProcedures.tsx";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -46,8 +49,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
